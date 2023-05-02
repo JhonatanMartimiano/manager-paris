f60b3306c0fb89d49125d2a02138e03c8e8b0e63
Jhonatan Martimiano <jhonatan_martimiano@hotmail.com>
2023-05-02 10:50:06 -0300

    Adicionando na busca de clientes por telefone e telefone internacional.

--- source/App/Admin/Clients.php
+++ source/App/Admin/Clients.php
@@ -42,7 +42,7 @@ class Clients extends Admin
 
         if (!empty($data["search"]) && str_search($data["search"]) != "all") {
             $search = str_search($data["search"]);
-            $clients = (new Client())->find("name LIKE CONCAT('%', :s, '%')", "s={$search}");
+            $clients = (new Client())->find("name LIKE CONCAT('%', :s, '%') OR phone LIKE CONCAT('%', :s, '%') OR phone_int LIKE CONCAT('%', :s, '%')", "s={$search}");
             if (!$clients->count()) {
                 $this->message->info("Sua pesquisa não retornou resultados")->flash();
                 redirect("/admin/clients/home");
